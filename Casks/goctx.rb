30f76ebddca4e2f2cd76916def8450b6d50af43d
Omer Preminger <omer@preminger.xyz>
2025-11-01 12:07:09 -0400

    Brew cask update for goctx version v0.4.1

--- Casks/goctx.rb
+++ Casks/goctx.rb
@@ -3,7 +3,7 @@ cask "goctx" do
   name "goctx"
   desc "Propagate context.Context through Go call graphs automatically."
   homepage "https://github.com/preminger/goctx"
-  version "0.4.0"
+  version "0.4.1"
 
   livecheck do
     skip "Auto-generated on release."
@@ -14,22 +14,22 @@ cask "goctx" do
   on_macos do
     on_intel do
       url "https://github.com/preminger/goctx/releases/download/v#{version}/goctx_#{version}_darwin_amd64.tar.gz"
-      sha256 "cf7d75a7296cf05c41494c8e61209fb494050fe501c05f11e9ca7ef819a5f7b2"
+      sha256 "2b41d7a9a9eac8f8ba79f1f8f7e2c366985818b115260e78fd7f75831d72b18a"
     end
     on_arm do
       url "https://github.com/preminger/goctx/releases/download/v#{version}/goctx_#{version}_darwin_arm64.tar.gz"
-      sha256 "a1a08d37a6e33dada250e085be5bd764fec80fe6d97377cf0df7ebbc9f236560"
+      sha256 "04addc65af21c0c2596ff15e1cdc406d9d72d0f7133c87516188536aa2c28ba2"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/preminger/goctx/releases/download/v#{version}/goctx_#{version}_linux_amd64.tar.gz"
-      sha256 "2ef245dad3054a9c515ab3e4b7268bea3d1e1b9d490c9c9327637e0b840219a7"
+      sha256 "c56e64948a87f6f1ef0fa706b0a7309e31f0e17b3499902e069f78b5c8738918"
     end
     on_arm do
       url "https://github.com/preminger/goctx/releases/download/v#{version}/goctx_#{version}_linux_arm64.tar.gz"
-      sha256 "effaea0489d2300b8e912075705b68b29bca9f2b5d972a972af57c37a6701561"
+      sha256 "b766450d0fc99cf72ce39b1e82dbb59213045dee99f1d31093e29fd866f92483"
     end
   end
 
